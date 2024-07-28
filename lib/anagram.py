677fd1bb05b1d078389c36d70d74a141b6ab848e
MatthewLeaman <leaman.30@osu.edu>
2024-07-28 17:07:17 -0400

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word="team"):
+        self.word=word
+    
+    def match(self, list):
+        new_list = []
+        for words in list:
+            if sorted(words) == sorted(self.word):
+                new_list.append(words)
+        return new_list
